cc79e8be07edcef9c3d60a942777f62c50ad79f3
Guillermo Llancaqueo <guillermo.llancaqueo@gmail.com>
2026-07-28 14:01:33 -0400

    fix: update apiBasePath to use localhost for production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBasePath: 'https://arriendos-ya-cndxdwcshsbrfpfg.chilecentral-01.azurewebsites.net/api',
+  apiBasePath: 'http://localhost:3000/api',
   tokenSAS: 'sp=r&st=2026-07-26T18:33:12Z&se=2026-08-29T02:48:12Z&spr=https&sv=2026-02-06&sr=c&sig=h9J%2FVFSTJzqMl5DCcexJq6TDgr4z0hur5X6n40WYgmw%3D',
 };
